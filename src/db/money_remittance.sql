2f67e50d703d058ac3706db8bd4df5edae1c0c40
ROMOXD <axceelfelis03@gmail.com>
2025-04-24 16:22:58 +0800

    Olympus

--- src/db/money_remittance.sql
+++ src/db/money_remittance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2025 at 03:58 PM
+-- Generation Time: Apr 24, 2025 at 10:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,7 +33,7 @@ CREATE TABLE `tbl_loans` (
   `u_username` varchar(50) NOT NULL,
   `loan_amount` decimal(10,2) NOT NULL,
   `loan_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `loan_status` enum('PENDING','PAID','REJECTED','') NOT NULL DEFAULT 'PENDING',
+  `loan_status` enum('PENDING','PAID','REJECTED','APPROVED') NOT NULL DEFAULT 'PENDING',
   `loan_description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -45,7 +45,9 @@ INSERT INTO `tbl_loans` (`loan_id`, `u_id`, `u_username`, `loan_amount`, `loan_d
 (1, 33, 'rhex123', 30000.00, '2025-04-15 12:31:52', 'REJECTED', 'Loan request submitted'),
 (2, 33, 'rhex123', 40000.00, '2025-04-15 12:52:24', 'REJECTED', 'Loan request submitted'),
 (3, 33, 'rhex123', 40000.00, '2025-04-15 12:55:17', 'PAID', 'Loan request submitted'),
-(4, 33, 'rhex123', 60000.00, '2025-04-15 12:58:11', 'PENDING', 'Loan request submitted');
+(4, 33, 'rhex123', 60000.00, '2025-04-15 12:58:11', 'PAID', 'Loan request submitted'),
+(5, 33, 'rhex123', 30000.00, '2025-04-24 07:54:19', 'APPROVED', 'Loan request submitted'),
+(6, 33, 'rhex123', 45000.00, '2025-04-24 08:09:44', 'APPROVED', 'Loan request submitted');
 
 -- --------------------------------------------------------
 
@@ -94,16 +96,16 @@ INSERT INTO `tbl_log` (`log_id`, `u_id`, `u_username`, `login_time`, `u_type`, `
 (24, 31, 'jay123', '2025-04-12 13:03:44', 'User Reset Their Password', 'Inactive', '2025-04-12 13:07:26', NULL),
 (25, 31, 'jay123', '2025-04-12 13:06:55', 'Success - User Action', 'Inactive', '2025-04-12 13:07:26', 'User Reset Their Password'),
 (26, 31, 'jay123', '2025-04-12 13:07:24', 'Success - User Login', 'Inactive', '2025-04-12 13:07:26', NULL),
-(27, 33, 'rhex123', '2025-04-12 13:32:58', 'Success - User Action', 'Active', NULL, 'New user registered: rhex123'),
+(27, 33, 'rhex123', '2025-04-12 13:32:58', 'Success - User Action', 'Inactive', '2025-04-24 07:54:23', 'New user registered: rhex123'),
 (28, 31, 'jay123', '2025-04-12 13:33:58', 'Success - User Login', 'Inactive', '2025-04-12 13:34:00', NULL),
 (30, 31, 'jay123', '2025-04-12 13:34:23', 'Success - User Login', 'Inactive', '2025-04-12 13:34:39', NULL),
 (31, 31, 'jay123', '2025-04-12 13:34:37', 'Success - User Action', 'Inactive', '2025-04-12 13:34:39', 'User Changed Their Password'),
 (32, 31, 'jay123', '2025-04-12 13:35:00', 'Success - User Login', 'Active', NULL, NULL),
 (33, 24, 'paran1234', '2025-04-12 13:35:22', 'Success - Admin Login', 'Inactive', '2025-04-12 13:35:50', NULL),
 (34, 24, 'paran1234', '2025-04-12 13:35:30', 'Admin', 'Inactive', '2025-04-12 13:35:50', 'Deleted user account with ID: 12'),
-(35, 33, 'rhex123', '2025-04-15 12:27:30', 'Success - User Login', 'Active', NULL, NULL),
-(36, 33, 'rhex123', '2025-04-15 12:28:58', 'Success - User Login', 'Active', NULL, NULL),
-(37, 33, 'rhex123', '2025-04-15 12:31:38', 'Success - User Login', 'Active', NULL, NULL),
+(35, 33, 'rhex123', '2025-04-15 12:27:30', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
+(36, 33, 'rhex123', '2025-04-15 12:28:58', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
+(37, 33, 'rhex123', '2025-04-15 12:31:38', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
 (38, 24, 'paran1234', '2025-04-15 12:33:20', 'Success - Admin Login', 'Inactive', '2025-04-15 12:33:33', NULL),
 (39, 24, 'paran1234', '2025-04-15 12:34:02', 'Success - Admin Login', 'Active', NULL, NULL),
 (40, 24, 'paran1234', '2025-04-15 12:34:44', 'Success - Admin Login', 'Active', NULL, NULL),
@@ -111,19 +113,32 @@ INSERT INTO `tbl_log` (`log_id`, `u_id`, `u_username`, `login_time`, `u_type`, `
 (42, 24, 'paran1234', '2025-04-15 12:43:08', 'Success - Admin Login', 'Active', NULL, NULL),
 (43, 24, 'paran1234', '2025-04-15 12:46:08', 'Success - Admin Login', 'Active', NULL, NULL),
 (44, 24, 'paran1234', '2025-04-15 12:49:35', 'Success - Admin Login', 'Active', NULL, NULL),
-(45, 33, 'rhex123', '2025-04-15 12:52:17', 'Success - User Login', 'Active', NULL, NULL),
+(45, 33, 'rhex123', '2025-04-15 12:52:17', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
 (46, 33, '', '2025-04-15 12:52:26', 'Success - User Action', 'Active', NULL, 'User Reset Their Password'),
-(47, 33, 'rhex123', '2025-04-15 12:55:09', 'Success - User Login', 'Active', NULL, NULL),
+(47, 33, 'rhex123', '2025-04-15 12:55:09', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
 (48, 33, '', '2025-04-15 12:55:18', 'Success - User Action', 'Active', NULL, 'User Loan a Money'),
-(49, 33, 'rhex123', '2025-04-15 12:58:04', 'Success - User Login', 'Active', NULL, NULL),
-(50, 33, 'rhex123', '2025-04-15 12:58:12', 'Success - User Action', 'Active', NULL, 'User Loan a Money'),
+(49, 33, 'rhex123', '2025-04-15 12:58:04', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
+(50, 33, 'rhex123', '2025-04-15 12:58:12', 'Success - User Action', 'Inactive', '2025-04-24 07:54:23', 'User Loan a Money'),
 (51, 24, 'paran1234', '2025-04-15 13:04:11', 'Success - Admin Login', 'Active', NULL, NULL),
 (52, 24, 'paran1234', '2025-04-15 13:06:27', 'Success - Admin Login', 'Active', NULL, NULL),
 (53, 24, 'paran1234', '2025-04-15 13:09:27', 'Success - Admin Login', 'Active', NULL, NULL),
 (54, 24, 'paran1234', '2025-04-15 13:09:30', 'Success - User Action', 'Active', NULL, 'Admin approved Loan ID: 3'),
 (55, 24, 'paran1234', '2025-04-15 13:11:40', 'Success - Admin Login', 'Active', NULL, NULL),
 (56, 24, 'paran1234', '2025-04-15 13:11:44', 'Success - User Action', 'Active', NULL, 'Admin rejected Loan ID: 2'),
-(57, 33, 'rhex123', '2025-04-15 13:56:19', 'Success - User Login', 'Active', NULL, NULL);
+(57, 33, 'rhex123', '2025-04-15 13:56:19', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
+(58, 24, 'paran1234', '2025-04-24 07:51:41', 'Success - Admin Login', 'Active', NULL, NULL),
+(59, 24, 'paran1234', '2025-04-24 07:51:45', 'Success - User Action', 'Active', NULL, 'Admin approved Loan ID: 4'),
+(60, 33, 'rhex123', '2025-04-24 07:54:08', 'Success - User Login', 'Inactive', '2025-04-24 07:54:23', NULL),
+(61, 33, 'rhex123', '2025-04-24 07:54:20', 'Success - User Action', 'Inactive', '2025-04-24 07:54:23', 'User Loan a Money'),
+(62, 24, 'paran1234', '2025-04-24 07:57:59', 'Success - Admin Login', 'Active', NULL, NULL),
+(63, 24, 'paran1234', '2025-04-24 07:58:50', 'Success - Admin Login', 'Active', NULL, NULL),
+(64, 24, 'paran1234', '2025-04-24 08:01:38', 'Success - Admin Login', 'Active', NULL, NULL),
+(65, 24, 'paran1234', '2025-04-24 08:07:24', 'Success - Admin Login', 'Active', NULL, NULL),
+(66, 24, 'paran1234', '2025-04-24 08:07:30', 'Success - User Action', 'Active', NULL, 'Admin approved Loan ID: 5'),
+(67, 33, 'rhex123', '2025-04-24 08:09:31', 'Success - User Login', 'Inactive', '2025-04-24 08:09:49', NULL),
+(68, 33, 'rhex123', '2025-04-24 08:09:45', 'Success - User Action', 'Inactive', '2025-04-24 08:09:49', 'User Loan a Money'),
+(69, 24, 'paran1234', '2025-04-24 08:09:57', 'Success - Admin Login', 'Active', NULL, NULL),
+(70, 24, 'paran1234', '2025-04-24 08:10:06', 'Success - User Action', 'Active', NULL, 'Admin approved Loan ID: 6');
 
 -- --------------------------------------------------------
 
@@ -194,13 +209,13 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_loans`
 --
 ALTER TABLE `tbl_loans`
-  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `loan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_log`
 --
 ALTER TABLE `tbl_log`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
